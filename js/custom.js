8f7d1777c6c007130fef14cd9aaccaac99b0bfdc
SIRΞNA AI <86911260+SIRENA-AI@users.noreply.github.com>
2021-08-07 00:35:19 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,12 @@
 $(window).on('load', function(){
 
 	"use strict";
- 
- 
+
+
 	/* ========================================================== */
 	/*   Navigation Background Color                              */
 	/* ========================================================== */
-	
+
 	$(window).on('scroll', function() {
 		if($(this).scrollTop() > 450) {
 			$('.navbar-fixed-top').addClass('opaque');
@@ -14,21 +14,21 @@ $(window).on('load', function(){
 			$('.navbar-fixed-top').removeClass('opaque');
 		}
 	});
- 
-	
+
+
 	/* ========================================================== */
 	/*   Hide Responsive Navigation On-Click                      */
 	/* ========================================================== */
-	
+
 	  $(".navbar-nav li a").on('click', function(event) {
 	    $(".navbar-collapse").collapse('hide');
 	  });
 
-	
+
 	/* ========================================================== */
 	/*   Navigation Color                                         */
 	/* ========================================================== */
-	
+
 	$('#navbarCollapse').onePageNav({
 		filter: ':not(.external)'
 	});
@@ -37,25 +37,25 @@ $(window).on('load', function(){
 	/* ========================================================== */
 	/*   SmoothScroll                                             */
 	/* ========================================================== */
-	
+
 	$(".navbar-nav li a, a.scrool").on('click', function(e) {
-		
+
 		var full_url = this.href;
 		var parts = full_url.split("#");
 		var trgt = parts[1];
 		var target_offset = $("#"+trgt).offset();
 		var target_top = target_offset.top;
-		
+
 		$('html,body').animate({scrollTop:target_top -70}, 1000);
 			return false;
-		
+
 	});
 
 
 	/* ========================================================== */
 	/*   Newsletter                                               */
 	/* ========================================================== */
-	
+
 	$('.newsletter-form').each( function(){
 		var form = $(this);
 		//form.validate();
@@ -71,13 +71,13 @@ $(window).on('load', function(){
 				e.preventDefault();
 			}
 		});
-	});		
-	
+	});
+
 
 	/* ========================================================== */
 	/*   Register Top Home Section                                */
 	/* ========================================================== */
-	
+
 	$('#register-form-home').each( function(){
 		var form = $(this);
 		//form.validate();
@@ -95,13 +95,13 @@ $(window).on('load', function(){
 				e.preventDefault();
 			}
 		});
-	})		
-	
+	})
+
 
 	/* ========================================================== */
 	/*   Contact                                                 */
 	/* ========================================================== */
-	
+
 	$('#contact-form').each( function(){
 		var form = $(this);
 		//form.validate();
@@ -124,7 +124,7 @@ $(window).on('load', function(){
 	})
 });
 
-	
+
 
 	/* ========================================================== */
 	/*   Popup-Gallery                                            */
@@ -134,26 +134,38 @@ $(window).on('load', function(){
 		gallery: {
 		  enabled:true
 		}
-	}); 
-	
+	});
+
 	$('.popup-gallery').find('a.popup2').magnificPopup({
 		type: 'image',
 		gallery: {
 		  enabled:true
 		}
-	}); 
- 
+	});
+
 	$('.popup-gallery').find('a.popup3').magnificPopup({
 		type: 'image',
 		gallery: {
 		  enabled:true
 		}
-	}); 
- 
+	});
+
 	$('.popup-gallery').find('a.popup4').magnificPopup({
 		type: 'iframe',
 		gallery: {
 		  enabled:false
 		}
-	});  
- 
+	});
+
+
+	/* ========================================================== */
+	/*   New Feautures                                            */
+	/* ========================================================== */
+	const genNumber = () => {
+	  document.querySelector("h6").style.setProperty("--percent", 372);
+		 document.querySelector("h7").style.setProperty("--percent", 3);
+		 		 document.querySelector("h8").style.setProperty("--percent", 42)
+	};
+
+	setInterval(genNumber, 2000);
+	setTimeout(genNumber);
